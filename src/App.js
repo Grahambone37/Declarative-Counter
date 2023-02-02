794630ab029c581aa65338ae0f214bc36a5c4336
Grahambone37 <electricgrunt37@gmail.com>
2023-02-01 23:27:03 -0800

    counter is done, and made github repo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+//at this point I'm too scared to delete stuff, so this cool logo is gonna stay
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  let [counter, changeCounter] = useState(1)
+  window.changeCounter = changeCounter
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <h1>{counter}</h1>
+          <h4>{counter} is my (currently) favorite number.</h4>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
